83af9d22b22bcb6d11c918321a726372e3023f1e
fredshu <fredshu.github@mailnull.com>
2017-07-29 18:09:17 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
 ## The makeCacheMatrix function creates a special "matrix" object that
 ## can cache its inverse.
 
